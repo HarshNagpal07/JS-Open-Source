c0fbfcf5529a7c6aaa46fc6474c0279ff43bba46
Harsh Nagpal <hnagpal056@gmail.com>
2025-10-01 23:32:04 +0530

    Update Reverse.js

--- Data-Structures/Array/Reverse.js
+++ Data-Structures/Array/Reverse.js
@@ -10,6 +10,6 @@ const Reverse = (arr) => {
   for (let i = 0, j = arr.length - 1; i < arr.length / 2; i++, j--)
     [arr[i], arr[j]] = [arr[j], arr[i]]
 
-  return arr
+  return arr;
 }
 export { Reverse }
